0efd7c6095505d2d9fc98263c669b5bc55a736a8
Farzana Juthi <107349665+JuthiMoni@users.noreply.github.com>
2022-09-19 09:42:10 +0600

    Update FileReadWrite.java

--- ExamSwing/src/examswing/FileReadWrite.java
+++ ExamSwing/src/examswing/FileReadWrite.java
@@ -28,6 +28,28 @@ public class FileReadWrite {
         Scanner input = new Scanner(file);
         System.out.println(input.nextLine());
         input.close();
+        
+        
+        
+        //txt file e likhe netbeans e output
+        
+                try {
+      File myObj = new File("C:\\Users\\user\\OneDrive\\Desktop\\java.txt");
+            Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+        
+        
+        
+        
+        
 
     }
 
